9586bcdff8cad23a99413d3948b277eb176c534e
lcolgithub <lcolgithub@gmail.com>
2023-08-28 13:27:37 +0100

    main function, begins maze solving

---
+++ main.py
@@ -0,0 +1,24 @@
+from graphics import Window
+from graphics import Point
+from graphics import Line
+from cell import Cell
+from maze import Maze
+
+def main():
+
+    num_rows = 30
+    num_cols = 40
+    margin = 50
+    screen_x = 2000
+    screen_y = 1500
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze.solve()
+
+    win.wait_for_close()
+
+
+main()
